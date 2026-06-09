0913959b475065ece1bc394dd3c81ec7b96128ff
Raju Gorai <gorairaju699@gmail.com>
2026-06-09 14:16:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Show surprise section
 function showSurprise() {
     document.getElementById("surprise").style.display = "block";
-    document.getElementById("music").play();
+
+    const music = document.getElementById("music");
+    music.currentTime = 0;
+
+    music.play().catch(error => {
+        console.log(error);
+        alert("Music could not be played.");
+    });
 }
 
 function showGallery() {
