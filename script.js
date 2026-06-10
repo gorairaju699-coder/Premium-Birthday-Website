a39a104cda9a80f9d420b7ce49668fe15e8ec6e3
Raju Gorai <gorairaju699@gmail.com>
2026-06-10 10:49:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Show surprise section
 function showSurprise() {
+
     document.getElementById("surprise").style.display = "block";
 
-    const music = document.getElementById("music");
-    music.currentTime = 0;
-
-    music.play().catch(error => {
-        console.log(error);
-        alert("Music could not be played.");
+    confetti({
+        particleCount: 200,
+        spread: 120,
+        origin: { y: 0.6 }
     });
-}
 
+    document.getElementById("music").play();
+}
 function showGallery() {
     document.getElementById("gallery").style.display = "block";
 }
