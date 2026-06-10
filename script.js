31df5e9c9ee03e539b26d95f6a789cec64b60b5b
Raju Gorai <gorairaju699@gmail.com>
2026-06-10 10:44:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function showSurprise() {
 function showGallery() {
     document.getElementById("gallery").style.display = "block";
 }
+const text = "You are the best friend ever ❤️";
+let i = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 80);
+    }
+}
+
+typeWriter();
