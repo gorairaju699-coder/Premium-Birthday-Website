d0f772ef133e127d86f7281072d2987c4b9313be
Raju Gorai <gorairaju699@gmail.com>
2026-06-16 11:32:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -215,4 +215,20 @@ window.addEventListener("load", function () {
     }, 2000);
 
 });
+document.addEventListener("mousemove", function(e){
+
+    const sparkle = document.createElement("div");
+
+    sparkle.className = "sparkle";
+
+    sparkle.style.left = e.pageX + "px";
+    sparkle.style.top = e.pageY + "px";
+
+    document.body.appendChild(sparkle);
+
+    setTimeout(() => {
+        sparkle.remove();
+    }, 800);
+
+});
   
